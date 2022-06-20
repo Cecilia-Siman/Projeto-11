69d03cce2b4b59ba9c53d46dd33083cc28be67e0
Cecilia Salema <ceciliasiman15@gmail.com>
2022-06-20 09:30:10 -0300

    login e disposição de tweets

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 import express from "express";
-import { request } from "express";
-import { response } from "express";
 import cors from "cors";
 
 const server = express();
@@ -8,13 +6,38 @@ server.use(cors());
 
 let user;
 let picture;
+let tweets = [
+    {
+        username: "bobesponja",
+        tweet: "eu amo o hub",
+        avatar:"https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg",
+    }
+];
 
-server.get('/sign-up',(request,response) => {
+
+server.post('/sign-up',(request,response) => {
     user = request.username;
     picture = request.avatar;
     response.send("OK");
-    console.log(user, picture);
+    console.log(request.username);
 });
+
+server.get('/tweets',(request,response) => {
+    response.send(tweets);
+});
+
+server.post('/tweets',(request,response) => {
+    let text = request.tweet;
+    let obj = {
+        username:user,
+        tweet:text,
+        avatar:picture,
+    }  
+    tweets.push(obj);
+    response.send(tweets);
+});
+
+
 
 server.get('/teste',(request,response) => {
     response.send("teste");
